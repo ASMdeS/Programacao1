1e1a8acfde9bd94775167f53413896ac6b981a1a
asms <asms@cin.ufpe.br>
2023-07-05 17:06:04 -0300

    a

---
+++ CInBlol.py
@@ -0,0 +1,34 @@
+time = 0
+pontos = 0
+nome = "a"
+Top = "a"
+Jungler = "a"
+Mid = "a"
+Adc = "a"
+Suporte = "a"
+
+while time < 5 and nome != "Bruna":
+    nome = input()
+    lane = input()
+    elo = input()
+    if nome == "Bruna":
+        print("LOL NÃO!!! TUDO MENOS LOL!!!")
+    elif nome == "Artur":
+        print("Ele tá meio enferrujado...")
+    if nome == ""
+    if elo == "Bronze":
+        pontos = pontos + 1
+    elif elo == "Prata":
+        pontos = pontos + 2
+    elif elo == "Ouro":
+        pontos = pontos + 4
+    elif elo == "Platina":
+        pontos = pontos + 6
+    elif elo == "Diamante":
+        pontos = pontos + 8
+    elif elo == "Desafiante":
+        pontos = pontos + 10
+    else:
+        print("Não conheço esse elo, então vou considerar que é um elo ruim.")
+
+
