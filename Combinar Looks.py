89ee243e091ce7a75009751b65e11f1ecf5bf874
asms <arthur.souza2005@hotmail.com>
2023-07-18 17:01:35 -0300

    18/07

--- Combinar Looks.py
+++ Combinar Looks.py
@@ -1,3 +1,4 @@
+#Opções de roupas
 opcoes_penteados = input().split(" - ")
 opcoes_tops = input().split(" - ")
 opcoes_bottoms = input().split(" - ")
@@ -7,15 +8,22 @@ print("Triagem das peças realizada com sucesso, pronta para iniciar mesclagem!"
 
 
 
-#escolha_penteados =
-#escolha_tops =
-#escolha_bottoms =
-#escolha_sapatos =
-barbie_gostou = False
+escolha_penteado =
+escolha_top =
+escolha_bottom =
+escolha_sapato =
+saida = False
 
+
+
+#Usando a máquina
 while saida == False:
     selecao_barbie = input().split()
+    if selecao_barbie[0] == "1<":
+
     print("Iniciando mesclagem...")
+    print(f"""O look gerado foi: 
+    cabelo {escolha_penteado}, {escolha_top} com {escolha_bottom} e {escolha_sapato} nos pés.""")
     opiniao_barbie = input()
     if opiniao_barbie == "Amei!!":
         if escolha_top == "camisa da seleção":
@@ -26,8 +34,3 @@ while saida == False:
     elif opiniao_barbie == "Melhor escolher eu mesma":
         print("Acho que estou precisando de ajustes, Ken :(")
         saida = True
-
-
-
-#Escolhendo penteados
-
