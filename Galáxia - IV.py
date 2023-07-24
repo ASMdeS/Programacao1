e31c591214646a606eb362e6b63b97eac179d445
asms <arthur.souza2005@hotmail.com>
2023-07-24 18:35:38 -0300

    24/07

--- Galáxia - IV.py
+++ Galáxia - IV.py
@@ -1,16 +1,33 @@
-capsulas = eval(input())
-posicao_explosao = input()
+#Inputs
+posicao_capsulas = eval(input())
+posicao_explosao = eval(input())
 raio_impacto = int(input())
 
-print(capsulas[0][0])
-def ponto_central():
-    return [sum(lista_x) / len(lista_x), sum(lista_y) / len(lista_y)]
+#Listas que serão usadas
+posicao_central = []
+lista_sobreviventes = []
 
-#def checar_sobrevivencia():
+#Ponto Central
+def ponto_central(lista_x, lista_y):
+    return [(sum(lista_x) / len(lista_x)), (sum(lista_y) / len(lista_y))]
 
+#Checar sobrevivência
+def checar_sobrevivencia(capsula, astronauta):
+    return raio_impacto < ((posicao_capsulas[capsula][astronauta][0] - posicao_explosao[0]) ** 2 + (posicao_capsulas[capsula][astronauta][1] - posicao_explosao[1]) ** 2) ** (1 / 2)
+
+#Resgatar os astronautas
 def resgatar_astronautas():
-    for coordenada in capsulas:
-        capsulas[int(coordenada)][0]
-        capsulas[int(coordenada)][0]
-        ponto_central()
+    for capsula in range(0, len(posicao_capsulas)):
+        lista_x = []
+        lista_y = []
+        for astronauta in range(0, len(posicao_capsulas[capsula])):
+            if checar_sobrevivencia(capsula, astronauta):
+                lista_x.append(posicao_capsulas[capsula][astronauta][0])
+                lista_y.append(posicao_capsulas[capsula][astronauta][1])
+                lista_sobreviventes.append(posicao_capsulas[capsula][astronauta])
+        if len(lista_x) > 0:
+            posicao_central.append(ponto_central(lista_x, lista_y))
+    return [len(lista_sobreviventes), posicao_central]
 
+#Printar
+print(resgatar_astronautas())
