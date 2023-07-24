43a80956aa0c70f717c30f67bbeba51fa1333aaf
asms <arthur.souza2005@hotmail.com>
2023-07-24 16:59:02 -0300

    24/07

--- Galáxia - IV.py
+++ Galáxia - IV.py
@@ -2,10 +2,15 @@ capsulas = eval(input())
 posicao_explosao = input()
 raio_impacto = int(input())
 
+print(capsulas[0][0])
+def ponto_central():
+    return [sum(lista_x) / len(lista_x), sum(lista_y) / len(lista_y)]
+
+#def checar_sobrevivencia():
+
 def resgatar_astronautas():
     for coordenada in capsulas:
-        capsulas[0][coordenada]
+        capsulas[int(coordenada)][0]
+        capsulas[int(coordenada)][0]
+        ponto_central()
 
-def checar_sobrevivencia():
-
-def ponto_central():
