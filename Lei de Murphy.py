f72f2cbca58735df4767c2888ed33ce3ac62a0d6
ASMdeS <arthur.souza2005@hotmail.com>
2023-07-25 15:10:25 -0300

    20/07

--- Lei de Murphy.py
+++ Lei de Murphy.py
@@ -1,12 +1,26 @@
 equacao_posfixa = input().split()
+equacao_infixa = []
 
 def conversao_notacao():
     for parte in equacao_posfixa:
-        equacao_infixa = []
+        print(parte)
         if parte.isdigit():
             equacao_infixa.append(parte)
         else:
-            equacao_infixa.insert(parte.index() - 1, parte)
+            if '(' in equacao_infixa:
+                equacao_infixa.insert(equacao_posfixa.index(parte) + 1, parte)
+
+            else:
+                equacao_infixa.insert(equacao_posfixa.index(parte) + 1, parte)
+                equacao_infixa.insert(equacao_posfixa.index(parte) - 2, '(')
+                equacao_infixa.insert(equacao_posfixa.index(parte) + 2, ')')
+
     print(equacao_infixa)
+    equacao_final = " ".join(equacao_infixa)
+    print(equacao_final)
+    resultado_final = eval(equacao_final)
+    print(resultado_final)
+
+
 
 conversao_notacao()
