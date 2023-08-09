886bd4a8a4959f3adce357a76d707c4da5599d0b
asms <arthur.souza2005@hotmail.com>
2023-08-09 20:24:59 -0300

    08/09

--- Link em busca do culpado.py
+++ Link em busca do culpado.py
@@ -1,11 +1,20 @@
 nome_suspeito = input()
 string_concat = input()
-encontrou_suspeito =
-
+encontrou_suspeito = None
+i = 0
+j = 1
 #Slice string
 
+while i < len(string_concat) and encontrou_suspeito != nome_suspeito:
+    while j < len(string_concat) + 1 and encontrou_suspeito != nome_suspeito:
+        encontrou_suspeito = string_concat[i:(j)]
+        print(encontrou_suspeito)
+        j += 1
+    #return substrings
+    i += 1
+    j = 1
 
-if
-    print(f'Encontrei, o culpado é o {nome_suspeito}!')
-else:
-    print()
+#if
+    #print(f'Encontrei, o culpado é o {nome_suspeito}!')
+#else:
+    #print()
--- teste.py
+++ teste.py
@@ -1,2 +1,10 @@
-print(len("01000000"))
-print(len("01000001"))
+def get_all_substrings(input_string):
+    substrings = []
+    for i in range(len(input_string)):
+        for j in range(i + 1, len(input_string) + 1):
+            substrings.append(input_string[i:j])
+    return substrings
+
+input_string = "abcd"
+substrings = get_all_substrings(input_string)
+print(substrings)
