def9e57bdae9d25267ee054581faf03d44af849c
asms <arthur.souza2005@hotmail.com>
2023-08-22 16:56:10 -0300

    22/08

--- Look at them!.py
+++ Look at them!.py
@@ -3,7 +3,8 @@ todos_famosos = dict()
 
 for i in range(0, quantidade_famosos):
     informacoes_famoso = input().split(" - ")
-    todos_famosos[informacoes_famoso[0]] = [informacoes_famoso[1], informacoes_famoso[2]]
+    todos_famosos[informacoes_famoso[0]] = [informacoes_famoso[1], [int(informacoes_famoso[2])]]
+
+todos_famosos = sorted(todos_famosos.items(), key=lambda x:x[1][0])
 
 print(todos_famosos)
-
