d56dd371a53499d5878e897d9bf845672d9eb93b
ASMdeS <arthur.souza2005@hotmail.com>
2023-09-01 23:35:21 -0300

    01/09

--- Preciso treinar pernas.py
+++ Preciso treinar pernas.py
@@ -1,23 +1,26 @@
 #Função que retornará lista de todos os trigamas
 def trigramas(frase_entrada, i, frase_tigrama):
     if i + 3 <= len(frase_entrada):
-        frase_considerada = frase_entrada[i:i+3]
         frase_tigrama.append(frase_entrada[i:i+3])
         return trigramas(frase_entrada, i + 1, frase_tigrama)
     else:
         return frase_tigrama
 
-#Função que irá comparar
+#Função que irá comparar o primeiro termo
 def comparar(entrada, buscada, i):
     print(entrada)
     print(buscada)
-    for trigrama in entrada[i]:
-        print(trigrama)
-        print(buscada[0])
-        if buscada[0] == trigrama:
+    for trigrama in entrada[list(entrada.keys())[i]]:
+        if buscada[list(buscada.keys())[0]] == trigrama:
             if buscada == entrada[entrada.index(trigrama):len(buscada)+1]:
-                return entrada.index(trigrama)
-    return comparar(entrada, buscada, i + 1)
+                return i
+    else:
+        return comparar(entrada, buscada, i + 1, j, contador_correto)
+
+def certeza(j, ):
+    if buscada[list(buscada.keys())[j]] == entrada[list(entrada.keys())[i]]:
+        return f
+    else:
 
 #Será usado
 frase_entrada = None
@@ -37,4 +40,5 @@ for i in range(0, quantidade_trechos):
     frase_buscada = input()
     dicionario_buscado[frase_buscada] = trigramas(frase_buscada.lower(), 0, [])
 
-comparar(dicionario_buscado)
+
+comparar(dicionario_trigamas, dicionario_buscado, 0)
