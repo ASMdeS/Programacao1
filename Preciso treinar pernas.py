0981ffb69cf5c8e45b2b7b2060c98b773c8d25f1
ASMdeS <arthur.souza2005@hotmail.com>
2023-09-02 16:05:42 -0300

    01/09

--- Preciso treinar pernas.py
+++ Preciso treinar pernas.py
@@ -7,20 +7,24 @@ def trigramas(frase_entrada, i, frase_tigrama):
         return frase_tigrama
 
 #Função que irá comparar o primeiro termo
-def comparar(entrada, buscada, i):
+def comparar(entrada, buscada, index_entrada, index_buscada, frase_entrada, frase_buscada, boolean):
     print(entrada)
     print(buscada)
-    for trigrama in entrada[list(entrada.keys())[i]]:
-        if buscada[list(buscada.keys())[0]] == trigrama:
-            if buscada == entrada[entrada.index(trigrama):len(buscada)+1]:
-                return i
+    if boolean is True:
+        print("ddddddddddddddddddddddddddddddddddd")
+        print(buscada[list(buscada.keys())[frase_buscada]][index_buscada])
+        print(entrada[list(entrada.keys())[frase_entrada]][index_entrada])
+        if buscada[list(buscada.keys())[frase_buscada]][index_buscada] == entrada[list(entrada.keys())[frase_entrada]][index_entrada]:
+            print(boolean)
+            return comparar(entrada, buscada, index_entrada + 1, index_buscada + 1, frase_entrada, frase_buscada, True)
     else:
-        return comparar(entrada, buscada, i + 1, j, contador_correto)
-
-def certeza(j, ):
-    if buscada[list(buscada.keys())[j]] == entrada[list(entrada.keys())[i]]:
-        return f
-    else:
+        for trigrama in entrada[list(entrada.keys())[frase_entrada]]:
+            print(trigrama)
+            print(buscada[list(buscada.keys())[0]][0])
+            if buscada[list(buscada.keys())[frase_buscada]][0] == trigrama:
+                print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                return comparar(entrada, buscada, index_entrada, index_buscada + 1, frase_entrada, frase_buscada, True)
+    return comparar(entrada, buscada, index_entrada, index_buscada, frase_entrada + 1, frase_buscada, False)
 
 #Será usado
 frase_entrada = None
@@ -41,4 +45,4 @@ for i in range(0, quantidade_trechos):
     dicionario_buscado[frase_buscada] = trigramas(frase_buscada.lower(), 0, [])
 
 
-comparar(dicionario_trigamas, dicionario_buscado, 0)
+print(comparar(dicionario_trigamas, dicionario_buscado, 0, 0, 0, 0, False))
