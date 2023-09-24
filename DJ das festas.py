242102ee015131f8368b23e8765aeb8e17a7491d
ASMdeS <arthur.souza2005@hotmail.com>
2023-09-24 00:11:50 -0300

    24/09

--- DJ das festas.py
+++ DJ das festas.py
@@ -1,6 +1,7 @@
 class playlist:
-    def __init__(self, nome, musicas):
+    def __init__(self, nome, genero, musicas):
         self.nome = nome
+        self.genero = genero
         self.musicas = musicas
 
     def adicionar(self, musica):
@@ -10,30 +11,65 @@ class playlist:
         self.musicas.pop()
 
     def reproduzir(self):
-        print(f'Play em {self.musicas}')
-        for elemento in musicas:
+        print(f'Play em {self.nome}')
+        for elemento in self.musicas:
             print(f'Tocando {elemento}...')
         print(f'Essa festa foi um sucesso!')
 
     def duracao(self):
-        return (len(musicas) * 3)
+        return (len(self.musicas) * 3)
 
+
+def ajustar_playlist():
+    duracao_atual = playlist_obj.duracao()
+    musicas_adicionadas = 0
+    musicas_removidas = 0
+
+    while duracao_atual > duracao_festa:
+        playlist_obj.remover()
+        duracao_atual = playlist_obj.duracao()
+        musicas_removidas += 1
+
+    while duracao_atual < duracao_festa:
+        duracao_atual += 3
+        musicas_adicionadas += 1
+
+    return musicas_adicionadas, musicas_removidas
+
+
+def correcao_playlist(musicas_faltando, musicas_sobrando):
+    if musicas_faltando > 0:
+        print(f'Precisaremos adicionar mais {musicas_faltando} músicas a playlist {playlist_obj.nome}')
+        for i in range(musicas_faltando):
+            musica = input()
+            playlist_obj.musicas.append(musica)
+    if musicas_sobrando > 0:
+        print(f'Precisaremos remover {musicas_sobrando} músicas da playlist {playlist_obj.nome}')
+
+
+# Crie uma lista de playlists
+lista_playlists = []
 lista_generos = []
 
 qtd_playlists = int(input())
-print(qtd_playlists)
 
 for i in range(0, qtd_playlists):
     nome = input()
     genero = input()
-    musicas = input()
-    lista_generos.append(genero)
-    genero = playlist(nome, musicas.split(', '))
+    musicas = input().split(', ')
+    playlist_obj = playlist(nome, genero, musicas)
+    lista_playlists.append(playlist_obj)
 
 genero_tocado = input()
 duracao_festa = int(input())
-for elemento in lista_generos:
-    if elemento == genero_tocado:
-        if elemento.duracao() < duracao_festa:
-            musicas_faltando =
+encontrou_playlist = False
 
+for i, playlist_obj in enumerate(lista_playlists):
+    if playlist_obj.genero == genero_tocado:
+        musicas_faltando, musicas_sobrando = ajustar_playlist()
+        correcao_playlist(musicas_faltando, musicas_sobrando)
+        playlist_obj.reproduzir()
+        encontrou_playlist = True
+
+if not encontrou_playlist:
+    print(f'Não tenho nenhuma playlist do gênero {genero_tocado}, infelizmente não poderei tocar')
--- teste.py
+++ teste.py
@@ -1,19 +1,19 @@
-def comparar(entrada, buscada, index_entrada, index_buscada, frase_entrada, frase_buscada, boolean):
-    print(buscada[list(buscada.keys())[frase_buscada]])
-    print(entrada[list(entrada.keys())[frase_entrada]])
-    if boolean is True:
-        print("ddddddddddddddddddddddddddddddddddd")
-        print(buscada[list(buscada.keys())[frase_buscada]][index_buscada])
-        print(entrada[list(entrada.keys())[frase_entrada]][index_entrada])
-        if buscada[list(buscada.keys())[frase_buscada]][index_buscada] == entrada[list(entrada.keys())[frase_entrada]][index_entrada]:
-            print(boolean)
-            return comparar(entrada, buscada, index_entrada + 1, index_buscada + 1, frase_entrada, frase_buscada, True)
-    else:
-        print(entrada[list(entrada.keys())[frase_entrada]])
-        for trigrama in entrada[list(entrada.keys())[frase_entrada]]:
-            print(trigrama)
-            print(buscada[list(buscada.keys())[0]][0])
-            if buscada[list(buscada.keys())[frase_buscada]][0] == trigrama:
-                print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-                return comparar(entrada, buscada, index_entrada, index_buscada + 1, frase_entrada, frase_buscada, True)
-    return comparar(entrada, buscada, index_entrada, index_buscada, frase_entrada + 1, frase_buscada, False)
+class MyClass:
+    def __init__(self, data):
+        self.data = data
+
+# List to store instances of MyClass
+instances = []
+
+# Get the number of instances to create
+num_instances = int(input("Enter the number of instances to create: "))
+
+# Create instances with user-provided data
+for i in range(num_instances):
+    data = input(f"Enter data for instance {i + 1}: ")
+    instance = MyClass(data)
+    instances.append(instance)
+
+# Access and use the instances
+for i, instance in enumerate(instances):
+    print(f"Data for instance {i + 1}: {instance.data}")
